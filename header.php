eb99cbd9fa3b9c6cf64aa5a4531f6d0d3f981310
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-17 14:57:36 +0530

    header

---
+++ header.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="head">
+        <a href="#">Loremipsum5869@gmail.com</a>
+        <div class="head-right">
+            <a href="#">+36-544-455-4547</a>
+            <hr>
+            <a href=""  ><i id="social" class="fa-brands fa-facebook-f fa-lg" style="color: #fff;"></i></a>
+            <a href="" ><i id="social" class="fa-brands fa-twitter fa-lg" style="color: #fff;"></i></a>
+            <a href="" ><i id="social" class="fa-brands fa-instagram fa-lg" style="color: #fff;"></i></a>
+        </div>
+    </div>
+
+    <nav>
+        <div class="logo">
+            <a href="#"><img src="img/logo.svg" alt=""></a>
+        </div>
+        <div class="menus">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Join Us</a></li>
+            </ul>
+        </div>
+
+        <div class="get-quote">
+            <a href="#">Get A Quote</a>
+        </div>
+    </nav>
+</header>
