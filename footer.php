37827a6b0f0d448a6fe0f7b66b2dd2bccba7e09c
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-18 18:01:09 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,83 @@
+<footer>
+    <div class="footer-section">
+        <div class="footer-main">
+            <div class="f-logo">
+                <img src="img/logo2.svg" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur<br> adipiscing elit. Suspendisse</p>
+
+            </div>
+
+            <div class="f-1">
+                <h2>Office Address</h2>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">809-810 Titaanium Business Hub Opp.<br>
+                            Akash Empire Beside White Temple<br>
+                            Bhimrad Surat -395017 </a></h4>
+                </div>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">+91 79988-79900</a></h4>
+                </div>
+            </div>
+            <div class="f-1">
+                <h2>Explore</h2>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">Home</a></h4>
+                </div>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">About Us</a></h4>
+                </div>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">Contact Us</a></h4>
+                </div>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">Join Us</a></h4>
+                </div>
+            </div>
+            <div class="f-1">
+                <h2>Our Branches</h2>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">Ahmedabad</a></h4>
+                </div>
+                <div class="f2">
+                    <img src="img/next.svg" alt="">
+                    <h4><a href="">Surat</a></h4>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="footer-social">
+           <a href=""> <img src="img/face.svg" alt=""></a>
+            <a href=""> <img src="img/link.svg" alt=""></a>
+                <a href=""> <img src="img/twit.svg" alt=""></a>
+                    <a href=""> <img src="img/socialbe.svg" alt=""></a>
+        </div>
+
+
+    </div>
+
+    <div class="footer-last">
+       <h1> &copy; 2023 All Rights Reserve</h1>
+        <div class="last-1">
+           <a href=""> <h1>Privacy</h1></a>
+            <hr>
+            <a href=""> <h1>Terms & Condition</h1></a>
+            <hr>
+            <a href=""> <h1>About Us</h1></a>
+
+        </div>
+    </div>
+
+
+
+
+
+</footer>
